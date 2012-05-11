3d021465461c71ac6e0c63324032683a05b7c92f
EasternStarGeek <easternstargeek@gmail.com>
2012-05-11 16:30:28 -0400

    arduino update" git commit -a -m "arduino update" help

--- FunWithThermistors/FunWithThermistors.ino
+++ FunWithThermistors/FunWithThermistors.ino
@@ -13,7 +13,7 @@ Project Description:
  The LUT has been derived from the manufacturer's published resistance table with the help of an
  Excel spreadsheet.
  
-  */
+ */
 
 
 // ################################################################################
@@ -57,19 +57,27 @@ void setup() {
 void loop() {
   // This is the main part of your project.  Put all continuously running code here:
 
-if (millis() > millisPrev + 2000) {
-  
-  tempC = getThermTemp(pinTherm) + 1.65;  // Add a Calibration Offset
-  tempF = (tempC * 1.8) + 32;
-  
-  Serial.print("TempC: ");
-  Serial.print(tempC);
-  Serial.print("  TempF: ");
-  Serial.println(tempF);
-  
-  millisPrev = millis();
-}
-  
+  if (millis() > millisPrev + 2000) {
+
+    tempC = getThermTemp(pinTherm); 
+
+    if (tempC > 998)
+      Serial.println("Error: Sensor reading over range!");
+    else if (tempC < -998)
+      Serial.println("Error: Sensor reading under range!");
+    else {
+
+      tempC = tempC + 1.65;  // Calibration offset
+      tempF = (tempC * 1.8) + 32;
+
+      Serial.print("TempC: ");
+      Serial.print(tempC);
+      Serial.print("  TempF: ");
+      Serial.println(tempF);
+
+    }
+    millisPrev = millis();  
+  }
 
 
 }  // END Loop Procedure
@@ -80,73 +88,76 @@ if (millis() > millisPrev + 2000) {
 // ################################################################################
 
 float getThermTemp (int thermPin)  {
-  
-/* The following lookup table contains the predicted ADC values for -20 deg C to + 69 deg C
+
+  /* The following lookup table contains the predicted ADC values for -20 deg C to + 69 deg C
    in a Voltage Divider Circuit with Ra=Thermistor and Rb=10K.
    +Vref---[Thermistor]---+--[10K]---GND
-                          |
-                         ADC @ thermPin
-
+   |
+   ADC @ thermPin
+   
    ADC = 1023 * 10000/(Rtherm+10000)
    ADC Values were externally calculated from the Thermistor Resistance Table
-*/
-const int LUT_Therm[90] = {
-  105, 110, 116, 121, 127, 133, 139, 145, 152, 159,   // -20C to -11C 
-  165, 173, 180, 187, 195, 203, 211, 219, 227, 236,   // -10C to -1C 
-  245, 254, 264, 273, 283, 293, 303, 313, 324, 334,   //  0C  to +9C 
-  345, 355, 366, 377, 388, 399, 410, 422, 433, 444,   //  10C to 19C 
-  455, 467, 478, 489, 500, 512, 523, 534, 545, 555,   //  20C to 29C 
-  566, 577, 588, 598, 608, 619, 629, 639, 648, 658,   //  30C to 39C 
-  667, 677, 686, 695, 704, 712, 721, 729, 737, 745,   //  40C to 49C 
-  753, 760, 768, 775, 782, 789, 795, 802, 808, 814,   //  50C to 59C 
-  820, 826, 832, 837, 843, 848, 853, 858, 863, 867    //  60C to 69C 
-};
+   */
+  const int LUT_Therm[90] = {
+    105, 110, 116, 121, 127, 133, 139, 145, 152, 159,   // -20C to -11C 
+    165, 173, 180, 187, 195, 203, 211, 219, 227, 236,   // -10C to -1C 
+    245, 254, 264, 273, 283, 293, 303, 313, 324, 334,   //  0C  to +9C 
+    345, 355, 366, 377, 388, 399, 410, 422, 433, 444,   //  10C to 19C 
+    455, 467, 478, 489, 500, 512, 523, 534, 545, 555,   //  20C to 29C 
+    566, 577, 588, 598, 608, 619, 629, 639, 648, 658,   //  30C to 39C 
+    667, 677, 686, 695, 704, 712, 721, 729, 737, 745,   //  40C to 49C 
+    753, 760, 768, 775, 782, 789, 795, 802, 808, 814,   //  50C to 59C 
+    820, 826, 832, 837, 843, 848, 853, 858, 863, 867    //  60C to 69C 
+  };
 
-float tempC;
-int ADC_Lo;
-int ADC_Hi;
-int Temp_Lo;
-int Temp_Hi;
+  float tempC;
+  int ADC_Lo;
+  int ADC_Hi;
+  int Temp_Lo;
+  int Temp_Hi;
 
-// get raw ADC value from Thermistor voltage divider circuit
-int thermValue = analogRead(thermPin);
+  // get raw ADC value from Thermistor voltage divider circuit
+  int thermValue = analogRead(thermPin);
 
-// Sensor Data bounds check
-    if (thermValue < LUT_Therm[0]) 
-      tempC = -999;  // Under-range dummy value
-    else if (thermValue > LUT_Therm[89])
-      tempC = 999;  // Over-range dummy value
-    else {
+  // Sensor Data bounds check
+  if (thermValue < LUT_Therm[0]) 
+    tempC = -999;  // Under-range dummy value
+  else if (thermValue > LUT_Therm[89])
+    tempC = 999;  // Over-range dummy value
+  else {
 
-      // if Sensor Value is within range...
-      for (int i=0; i <= 89; i++){
-        if (LUT_Therm[i] > thermValue) {  // Look for a match in the lookup table
-          int ADC_Hi = LUT_Therm[i];
-          int Temp_Hi = i - 20;
+    // if Sensor Value is within range...
+    for (int i=0; i <= 89; i++){
+      if (LUT_Therm[i] > thermValue) {  // Look for a match in the lookup table
+        int ADC_Hi = LUT_Therm[i];
+        int Temp_Hi = i - 20;
 
-          if (i != 0)  {
-            int ADC_Lo = LUT_Therm[i-1];
-            int Temp_Lo = i - 21;
-          }
-          else  {
-            ADC_Lo = LUT_Therm[i];
-            Temp_Lo = i - 20;
-          }
-
-          // Interpolate the temperature value for greater precision
-          tempC = float( map(thermValue, ADC_Lo, ADC_Hi, Temp_Lo*100, Temp_Hi*100) )/100;
-          
-          break;  // exit for-next loop after the match is detected
+        if (i != 0)  {
+          int ADC_Lo = LUT_Therm[i-1];
+          int Temp_Lo = i - 21;
         }
+        else  {
+          ADC_Lo = LUT_Therm[i];
+          Temp_Lo = i - 20;
+        }
+
+        // Interpolate the temperature value for greater precision
+        tempC = float( map(thermValue, ADC_Lo, ADC_Hi, Temp_Lo*100, Temp_Hi*100) )/100;
+
+        break;  // exit for-next loop after the match is detected
       }
     }
-    return (tempC);
   }
+  return (tempC);
+}
 
 
 
 
 // END Outboard Function Area
+
+
+
 
 
 
